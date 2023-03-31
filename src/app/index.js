aba43738c7ce90658ac1b82ac907b3e71c39f2fc
xxy <1696944145@qq.com>
2023-03-31 21:10:14 +0800

    解析body与添加服务层

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,9 @@
 const Koa = require('koa'); // koa是一个类，调用需要实例化
-const app = new Koa();
-
-
+const { koaBody } = require('koa-body');
 const userRouter = require('../router/user.router.js');
+
+const app = new Koa();
+app.use(koaBody());
 app.use(userRouter.routes());
 app.use((cxt,next) => {
     cxt.body = 'hello api';
--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,6 +1,14 @@
+const { createUser } = require('../service/user.service.js')
+
 class UserController {
     async register(ctx,next) {
-        ctx.body = '用户注册成功'
+        // 1. 获取数据
+        const { user_name,password } = ctx.request.body;
+        // 2. 操作数据
+        const res = await createUser(user_name,password);
+        console.log(res);
+        // 3. 返回结果
+        ctx.body = '用户注册成功';
     }
     async login(ctx,next) {
         ctx.body = '用户登录成功'
